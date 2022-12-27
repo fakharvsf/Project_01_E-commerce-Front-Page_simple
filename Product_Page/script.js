dc8d1742604d7b391c80ba1bd7864530bd25aba3
Fakhar Abbas <afakhar363@gmail.com>
2022-12-28 00:26:01 +0500

    Some Inportang Changes

--- Product_Page/script.js
+++ Product_Page/script.js
@@ -7,3 +7,13 @@ Home.addEventListener("click", () => {
 Catagory.addEventListener("click", () => {
   window.location.replace("../Catalog_Page/index.html");
 });
+// const FigureZoom = document.querySelector(".Figure-zoom");
+
+// function zoom(e) {
+//   var zoomer = e.currentTarget;
+//   e.offsetX ? (offsetX = e.offsetX) : (offsetX = e.touches[0].pageX);
+//   e.offsetY ? (offsetY = e.offsetY) : (offsetX = e.touches[0].pageX);
+//   x = (offsetX / zoomer.offsetWidth) * 100;
+//   y = (offsetY / zoomer.offsetHeight) * 100;
+//   zoomer.style.backgroundPosition = x + "% " + y + "%";
+// }
